3ff9b514c72d35e5632708bb69bc24d777cf7e76
Eric <luckwuwenjun@outlook.com>
2022-03-04 11:20:18 +0800

    修改插件github的镜像网站

--- lua/basic/plugins.lua
+++ lua/basic/plugins.lua
@@ -1,4 +1,28 @@
 local packer = require("packer")
+-- 配置github 国内镜像
+packer.init(
+{
+    git = {
+    cmd = 'git', -- The base command for git operations
+    subcommands = { -- Format strings for git subcommands
+      update         = 'pull --ff-only --progress --rebase=false',
+      install        = 'clone --depth %i --no-single-branch --progress',
+      fetch          = 'fetch --depth 999999 --progress',
+      checkout       = 'checkout %s --',
+      update_branch  = 'merge --ff-only @{u}',
+      current_branch = 'branch --show-current',
+      diff           = 'log --color=never --pretty=format:FMT --no-show-signature HEAD@{1}...HEAD',
+      diff_fmt       = '%%h %%s (%%cr)',
+      get_rev        = 'rev-parse --short HEAD',
+      get_msg        = 'log --color=never --pretty=format:FMT --no-show-signature HEAD -n 1',
+      submodules     = 'submodule update --init --recursive --progress'
+    },
+    depth = 1, -- Git clone depth
+    clone_timeout = 60, -- Timeout, in seconds, for git clones
+    default_url_format = 'https://hub.xn--gzu630h.xn--kpry57d/%s' -- Lua format string used for "aaa/bbb" style plugins
+  }
+}
+)
 packer.startup(
     {
         -- 所有插件的安装都书写在 function 中
