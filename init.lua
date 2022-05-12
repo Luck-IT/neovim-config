c2e2e07dab19ae7c454045d0b4fac3b96086b95e
Eric Wu <wuwj107@chinaunicom.cn>
2022-05-12 09:48:09 +0000

    新neovim配置

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- 基础配置
+require("basic")
+-- 快捷键映射
+require("keybindings")
+-- Packer 插件管理
+require("plugins")
+-- 主题设置
+require("colorscheme")
+-- 插件配置
+require("plugin-config.nvim-tree")
---
+++ lua/plugins.lua
@@ -0,0 +1,55 @@
+local packer = require('packer')
+
+packer.startup(
+{
+  function(use)
+    --Packer 自身
+    use ("wbthomason/packer.nvim")
+    --------- colorschemes -------------------------
+    -- tokyonight
+    use("folke/tokyonight.nvim")
+    -- OceanicNext
+    use("mhartington/oceanic-next")
+    -- gruvbox
+    use({ "ellisonleao/gruvbox.nvim", requires = "rktjmp/lush.nvim" })
+    -- zephyr 暂时不推荐，详见上边解释
+    -- use("glepnir/zephyr-nvim")
+    -- nord
+    use("shaunsingh/nord.nvim")
+    -- onedark
+    use("ful1e5/onedark.nvim")
+    -- nightfox
+    use("EdenEast/nightfox.nvim")
+    ------------------------------------------------
+    -- nvim-tree
+    use({"kyazdani42/nvim-tree.lua",requires = "kyazdani42/nvim-web-devicons"})
+  end,
+  config = {
+    max_jobs = 16,
+    -- 自定义插件源，github网站无法访问可以使用
+    git = {
+          --  default_url_format = "https://hub.fastgit.xyz/%s",
+          --  default_url_format = "https://mirror.ghproxy.com/https://github.com/%s",
+          --  default_url_format = "https://gitcode.net/mirrors/%s",
+          --  default_url_format = "https://gitclone.com/github.com/%s"
+    },
+    display = {
+      open_fn = function()
+        return require("packer.util").float({ border = "single" })
+      end,
+    }
+  }
+}
+)
+
+
+-- 每次保存plugin.lua 文件自动安装插件
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
